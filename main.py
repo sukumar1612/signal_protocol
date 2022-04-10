7b54fe1b871c81a7d6731ac2c59be9c18c245423
sukumar ganesan <suku1612@gmail.com>
2022-04-10 16:47:30 +0530

    feat: added double ratchet mechanism

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    print("hello world")
+from src.x3dh.key_bundles import create_new_ephemeral_key_bundle, create_new_pre_key_bundle
+from src.x3dh.session import Session, Mode
+
+if __name__ == '__main__':
+    # alice
+
+    alice = create_new_ephemeral_key_bundle()
+    alice_ephemeral_keys = alice.publish_keys()
+    # bob
+
+    bob = create_new_pre_key_bundle(number_of_onetime_pre_keys=10)
+    bob_pre_key_bundle = bob.publish_keys()
+    # key exchange
+
+    x1 = Session(pre_key_bundle=bob, ephemeral_key_bundle=alice_ephemeral_keys, mode=Mode.bob)
+    x2 = Session(pre_key_bundle=bob_pre_key_bundle, ephemeral_key_bundle=alice, mode=Mode.alice)
+
--- src/x3dh/session.py
+++ src/x3dh/session.py
@@ -1,11 +1,19 @@
+import enum
+
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PublicKey
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 from src.x3dh.key_bundles import PreKeyBundlePrivate, PreKeyBundlePublic, EphemeralKeyBundlePrivate, \
-    create_new_pre_key_bundle, EphemeralKeyBundlePublic, create_new_ephemeral_key_bundle
+    create_new_pre_key_bundle, EphemeralKeyBundlePublic, create_new_ephemeral_key_bundle, generate_keys
 
 
-def key_derivation_function(shared_key: bytes):
+class Mode(enum.Enum):
+    alice = "alice"
+    bob = "bob"
+
+
+def key_derivation_function(shared_key: bytes, salt: bytes = None):
     return HKDF(
         algorithm=hashes.SHA256(),
         length=128,
@@ -14,25 +22,49 @@ def key_derivation_function(shared_key: bytes):
     ).derive(shared_key)
 
 
-def generate_shared_key_from_ephemeral_key(pre_key_bundle_private: PreKeyBundlePrivate,
-                                           ephemeral_key_bundle_public: EphemeralKeyBundlePublic):
-    DH1 = pre_key_bundle_private.IK_private.exchange(ephemeral_key_bundle_public.ephemeral_key_public)
-    DH2 = pre_key_bundle_private.SPK_private.exchange(ephemeral_key_bundle_public.ephemeral_key_public)
-    DH3 = pre_key_bundle_private.SPK_private.exchange(ephemeral_key_bundle_public.IK_public)
-    DH4 = pre_key_bundle_private.OP_key_private[0].exchange(ephemeral_key_bundle_public.ephemeral_key_public)
-    pre_key_bundle_private.OP_key_private.pop(0)
+class Session:
+    def __init__(self, pre_key_bundle, ephemeral_key_bundle, mode: Mode):
+        self.mode = mode
+        self.shared_key: bytes = None
+        self.salt: bytes = None
+        self.DH_key_private, self.DH_key_public = generate_keys()
+        if self.mode == Mode.alice:
+            self.shared_key = Session.generate_shared_key_from_pre_key_bundle(
+                pre_key_bundle_public=pre_key_bundle, ephemeral_key_bundle_private=ephemeral_key_bundle)
+        else:
+            self.shared_key = Session.generate_shared_key_from_ephemeral_key(pre_key_bundle_private=pre_key_bundle,
+                                                                             ephemeral_key_bundle_public=ephemeral_key_bundle)
 
-    return key_derivation_function(DH1 + DH2 + DH3 + DH4)
+    @staticmethod
+    def generate_shared_key_from_pre_key_bundle(pre_key_bundle_public: PreKeyBundlePublic,
+                                                ephemeral_key_bundle_private: EphemeralKeyBundlePrivate):
+        DH1 = ephemeral_key_bundle_private.ephemeral_key_private.exchange(pre_key_bundle_public.IK_public)
+        DH2 = ephemeral_key_bundle_private.ephemeral_key_private.exchange(pre_key_bundle_public.SPK_public)
+        DH3 = ephemeral_key_bundle_private.IK_private.exchange(pre_key_bundle_public.SPK_public)
+        DH4 = ephemeral_key_bundle_private.ephemeral_key_private.exchange(pre_key_bundle_public.OP_key_public)
 
+        return key_derivation_function(DH1 + DH2 + DH3 + DH4)
 
-def generate_shared_key_from_pre_key_bundle(pre_key_bundle_public: PreKeyBundlePublic,
-                                            ephemeral_key_bundle_private: EphemeralKeyBundlePrivate):
-    DH1 = ephemeral_key_bundle_private.ephemeral_key_private.exchange(pre_key_bundle_public.IK_public)
-    DH2 = ephemeral_key_bundle_private.ephemeral_key_private.exchange(pre_key_bundle_public.SPK_public)
-    DH3 = ephemeral_key_bundle_private.IK_private.exchange(pre_key_bundle_public.SPK_public)
-    DH4 = ephemeral_key_bundle_private.ephemeral_key_private.exchange(pre_key_bundle_public.OP_key_public)
+    @staticmethod
+    def generate_shared_key_from_ephemeral_key(pre_key_bundle_private: PreKeyBundlePrivate,
+                                               ephemeral_key_bundle_public: EphemeralKeyBundlePublic):
+        DH1 = pre_key_bundle_private.IK_private.exchange(ephemeral_key_bundle_public.ephemeral_key_public)
+        DH2 = pre_key_bundle_private.SPK_private.exchange(ephemeral_key_bundle_public.ephemeral_key_public)
+        DH3 = pre_key_bundle_private.SPK_private.exchange(ephemeral_key_bundle_public.IK_public)
+        DH4 = pre_key_bundle_private.OP_key_private[0].exchange(ephemeral_key_bundle_public.ephemeral_key_public)
+        pre_key_bundle_private.OP_key_private.pop(0)
 
-    return key_derivation_function(DH1 + DH2 + DH3 + DH4)
+        return key_derivation_function(DH1 + DH2 + DH3 + DH4)
+
+    def symmetric_key_ratchet(self):
+        self.shared_key = key_derivation_function(self.shared_key, self.salt)
+
+    def diffie_hellman_ratchet(self, public_key: X25519PublicKey):
+        self.salt = self.DH_key_private.exchange(public_key)
+        self.symmetric_key_ratchet()
+
+    def update_diffie_hellman_keys(self):
+        self.DH_key_private, self.DH_key_public = generate_keys()
 
 
 if __name__ == '__main__':
@@ -43,11 +75,16 @@ if __name__ == '__main__':
     bob = create_new_pre_key_bundle(number_of_onetime_pre_keys=10)
     bob_pre_key_bundle = bob.publish_keys()
     # key exchange
-    x1 = generate_shared_key_from_ephemeral_key(pre_key_bundle_private=bob,
-                                                ephemeral_key_bundle_public=alice_ephemeral_keys)
-    x2 = generate_shared_key_from_pre_key_bundle(pre_key_bundle_public=bob_pre_key_bundle,
-                                                 ephemeral_key_bundle_private=alice)
+    x1 = Session(pre_key_bundle=bob, ephemeral_key_bundle=alice_ephemeral_keys, mode=Mode.bob)
+    x2 = Session(pre_key_bundle=bob_pre_key_bundle, ephemeral_key_bundle=alice, mode=Mode.alice)
 
-    print(x1)
-    print(x2)
-    print(x1 == x2)
+    for i in range(10):
+        x1.diffie_hellman_ratchet(x2.DH_key_public)
+        x2.diffie_hellman_ratchet(x1.DH_key_public)
+
+        x1.update_diffie_hellman_keys()
+        x2.update_diffie_hellman_keys()
+
+        print(x1.shared_key)
+        print(x2.shared_key)
+        print(x1.shared_key == x2.shared_key)
